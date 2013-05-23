b600604bfe5fd1529c4a86b9448614b7eba66e87
Adam Hinz <hinz.adam@gmail.com>
2013-05-22 21:43:02 -0400

    Singly linked list
    Supports the following:
    - scons
    - shead
    - stail
    - stoclj
    - sreverse
    - sinsert
    - sdelete

--- src/datastructures/core.clj
+++ src/datastructures/core.clj
@@ -1,8 +1,67 @@
 (ns datastructures.core)
 
-;; x
+;;;;;;;; Singly linked list ;;;;;;;;;;
+(deftype SCons [x xs])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def SNil (SCons. nil nil))
+
+(defn scons
+  "Concat a new element to a singly linked list"
+  [e l]
+  (SCons. e l))
+
+(defn shead
+  "Get the first element of a singly linked list"
+  [l]
+  (.x l))
+
+(defn stail
+  "Get the remaining elements in the sequence"
+  [l]
+  (.xs l))
+
+(defn stoclj
+  "Convert a singly linked list into a vector"
+  ([l]
+     (stoclj l []))
+
+  ([l z]
+     (if (= l SNil)
+       z
+       (recur (stail l) (conj z (shead l))))))
+
+(defn sreverse
+  "Reverse a singly linked list"
+  ([l] (sreverse l SNil))
+  ([l s]
+     (if (= l SNil)
+       s
+       (sreverse (stail l) (scons (shead l) s)))))
+
+(defn sinsert
+  "Insert into value v into a linked list at index i"
+  ([l v i] (sinsert l v i SNil))
+  ([l v i x]
+     (cond
+      (= l SNil)
+      (sreverse x)
+
+      (= i 0)
+      (recur (stail l) v (- i 1) (scons (shead l) (scons v x)))
+
+      :else
+      (recur (stail l) v (- i 1) (scons (shead l) x)))))
+
+(defn sdelete
+  "Delete a value at index i from the linked list"
+  ([l i] (sdelete l i SNil))
+  ([l i x]
+     (cond
+      (= SNil l)
+      (sreverse x)
+
+      (= i 0)
+      (recur (stail l) (- i 1)  x)
+
+      :else
+      (recur (stail l) (- i 1) (scons (shead l) x)))))
--- test/datastructures/core_test.clj
+++ test/datastructures/core_test.clj
@@ -2,6 +2,25 @@
   (:use clojure.test
         datastructures.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest single-list
+
+  ; 9 : 10 : 13 : 11 : 99 : Nil
+  (def l (scons 9 (scons 10 (scons 13 (scons 11 (scons 99 SNil))))))
+
+  (testing "Basic list operations"
+    (is (= (stoclj l) [9 10 13 11 99]))
+    (is (= (shead l) 9))
+    (is (= (shead (stail l)) 10))
+    (is (= (stoclj (stail l)) [10 13 11 99])))
+
+  (testing "Reverse"
+    (is (= (stoclj (sreverse l)) [99 11 13 10 9])))
+
+  (testing "Insert"
+    (is (= (stoclj (sinsert l 5 0)) [5 9 10 13 11 99]))
+    (is (= (stoclj (sinsert l 5 1)) [9 5 10 13 11 99]))
+    (is (= (stoclj (sinsert l 5 3)) [9 10 13 5 11 99])))
+
+  (testing "Delete"
+    (is (= (stoclj (sdelete l 0)) [10 13 11 99]))
+    (is (= (stoclj (sdelete l 2)) [9 10 11 99]))))
